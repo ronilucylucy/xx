c1822833f2e7836d2f1b896cbfb88f5ad96d1bf3
ronilucylucy <ronilucylucy@gmail.com>
2025-02-02 13:43:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
+///selectors
 const sliderBtn = document.getElementById("sliderBtn");
 const body = document.querySelector(".body");
 
-sliderBtn.onclick = function () {
+///state
+const theme = localStorage.getItem("theme");
+
+///on mount
+theme && document.body.classList.add(theme);
+
+///handlers
+const handleThemeToggle = () => {
   document.body.classList.toggle("dark-theme");
+
+  if (document.body.classList.contains("dark-theme")) {
+    localStorage.setItem("theme", "dark-theme");
+  } else {
+    localStorage.removeItem("theme");
+  }
 };
+///events
+
+sliderBtn.addEventListener("click", handleThemeToggle);
